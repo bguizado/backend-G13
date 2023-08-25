c889fedb5203ec3077fbd352d3dfb727dc30fb2d
bguizado <brando.guizado.87@gmail.com>
2023-08-24 20:14:47 -0500

    colocando host1

--- dulceria/dulceria/settings.py
+++ dulceria/dulceria/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://dulceria-brando.onrender.com',
+ALLOWED_HOSTS = ['dulceria-brando.onrender.com',
                  '127.0.0.1',
                  'localhost']
 
